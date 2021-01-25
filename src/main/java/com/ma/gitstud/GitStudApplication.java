68ba08ec4b35be957d9a656ac7bd6be9afd1b07e
935020579@qq.com <935020579@qq.com>
2021-01-25 09:25:02 +0800

    first commit

---
+++ src/main/java/com/ma/gitstud/GitStudApplication.java
@@ -0,0 +1,13 @@
+package com.ma.gitstud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ma/gitstud/GitStudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.gitstud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
